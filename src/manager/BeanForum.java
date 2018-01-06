29ce8bbdd2ef7e4a785314025aa727104d8dc06f
Joao Santana <wozefacu@cartelera.org>
2018-01-06 11:57:19 -0200

    first commit

--- src/manager/BeanForum.java
+++ src/manager/BeanForum.java
@@ -3,11 +3,16 @@ package manager;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import com.sun.faces.config.FacesConfigInfo;
 
 import entity.Assunto;
 import entity.Resposta;
+import persistence.AssuntoDao;
 
 @ManagedBean(name = "mb")
 @RequestScoped
@@ -20,7 +25,14 @@ public class BeanForum implements Serializable {
 
 	private List<Assunto> listaAssuntos;
 
+	
+	public BeanForum() {
+		assunto = new Assunto();
+		resposta = new Resposta();
+	}
+	
 	public List<Assunto> getListaAssuntos() {
+		listaAssuntos = new AssuntoDao().findAll();
 		return listaAssuntos;
 	}
 
@@ -50,6 +62,22 @@ public class BeanForum implements Serializable {
 
 	public void setLogado(Assunto logado) {
 		this.logado = logado;
+	}
+	
+	
+	public void gravarPrimeira() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		try {
+			
+			AssuntoDao dao = new AssuntoDao();
+			dao.create(assunto);
+			assunto = new Assunto();
+			
+			fc.addMessage(null,  new FacesMessage("Dados Gravados"));
+			
+		} catch (Exception e) {
+			fc.addMessage(null, new FacesMessage("Error : " + e.getMessage()));
+		}
 	}
 
 }
